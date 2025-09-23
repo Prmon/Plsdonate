5ef1d0e050dc0c7384e9b622c146a5e2ab0305c7
Prmon <101718039+Prmon@users.noreply.github.com>
2025-09-23 01:34:17 +0000

    Add files via upload

---
+++ projects/DUCk/DUCk/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace DUCk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.GunaAni = new Guna.UI2.WinForms.Guna2AnimateWindow(this.components);
+            this.Thethingthatmakesround = new Guna.UI2.WinForms.Guna2BorderlessForm(this.components);
+            this.DisplatN = new System.Windows.Forms.Label();
+            this.BtnInject = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2HtmlLabel2 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.Close1 = new Guna.UI2.WinForms.Guna2ImageButton();
+            this.SuspendLayout();
+            // 
+            // GunaAni
+            // 
+            this.GunaAni.AnimationType = Guna.UI2.WinForms.Guna2AnimateWindow.AnimateWindowType.AW_BLEND;
+            this.GunaAni.TargetForm = this;
+            // 
+            // Thethingthatmakesround
+            // 
+            this.Thethingthatmakesround.AnimateWindow = true;
+            this.Thethingthatmakesround.BorderRadius = 50;
+            this.Thethingthatmakesround.ContainerControl = this;
+            this.Thethingthatmakesround.DockIndicatorTransparencyValue = 0.6D;
+            this.Thethingthatmakesround.ResizeForm = false;
+            this.Thethingthatmakesround.TransparentWhileDrag = true;
+            // 
+            // DisplatN
+            // 
+            this.DisplatN.AutoSize = true;
+            this.DisplatN.Font = new System.Drawing.Font("Minecraft", 15.25F);
+            this.DisplatN.ForeColor = System.Drawing.Color.White;
+            this.DisplatN.Location = new System.Drawing.Point(12, 9);
+            this.DisplatN.Name = "DisplatN";
+            this.DisplatN.Size = new System.Drawing.Size(62, 25);
+            this.DisplatN.TabIndex = 0;
+            this.DisplatN.Text = "DUCk";
+            // 
+            // BtnInject
+            // 
+            this.BtnInject.Animated = true;
+            this.BtnInject.BorderColor = System.Drawing.Color.White;
+            this.BtnInject.BorderRadius = 1;
+            this.BtnInject.BorderThickness = 1;
+            this.BtnInject.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.BtnInject.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.BtnInject.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.BtnInject.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.BtnInject.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            this.BtnInject.Font = new System.Drawing.Font("Minecraft", 15.25F);
+            this.BtnInject.ForeColor = System.Drawing.Color.White;
+            this.BtnInject.Location = new System.Drawing.Point(207, 155);
+            this.BtnInject.Name = "BtnInject";
+            this.BtnInject.Size = new System.Drawing.Size(180, 45);
+            this.BtnInject.TabIndex = 1;
+            this.BtnInject.Text = "INJECT";
+            this.BtnInject.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // guna2HtmlLabel1
+            // 
+            this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel1.Font = new System.Drawing.Font("Minecraft", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel1.ForeColor = System.Drawing.Color.White;
+            this.guna2HtmlLabel1.Location = new System.Drawing.Point(12, 315);
+            this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            this.guna2HtmlLabel1.Size = new System.Drawing.Size(77, 15);
+            this.guna2HtmlLabel1.TabIndex = 3;
+            this.guna2HtmlLabel1.Text = "Owner DANQT";
+            this.guna2HtmlLabel1.TextAlignment = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // guna2HtmlLabel2
+            // 
+            this.guna2HtmlLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel2.Font = new System.Drawing.Font("Minecraft", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel2.ForeColor = System.Drawing.Color.White;
+            this.guna2HtmlLabel2.Location = new System.Drawing.Point(12, 303);
+            this.guna2HtmlLabel2.Name = "guna2HtmlLabel2";
+            this.guna2HtmlLabel2.Size = new System.Drawing.Size(76, 15);
+            this.guna2HtmlLabel2.TabIndex = 4;
+            this.guna2HtmlLabel2.Text = "Homie Execpt";
+            this.guna2HtmlLabel2.TextAlignment = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // Close1
+            // 
+            this.Close1.CheckedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.Close1.HoverState.ImageSize = new System.Drawing.Size(64, 64);
+            this.Close1.Image = ((System.Drawing.Image)(resources.GetObject("Close1.Image")));
+            this.Close1.ImageOffset = new System.Drawing.Point(0, 0);
+            this.Close1.ImageRotate = 0F;
+            this.Close1.ImageSize = new System.Drawing.Size(10, 10);
+            this.Close1.Location = new System.Drawing.Point(574, 9);
+            this.Close1.Name = "Close1";
+            this.Close1.PressedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.Close1.Size = new System.Drawing.Size(24, 22);
+            this.Close1.TabIndex = 5;
+            this.Close1.Click += new System.EventHandler(this.Close1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            this.ClientSize = new System.Drawing.Size(610, 342);
+            this.Controls.Add(this.Close1);
+            this.Controls.Add(this.guna2HtmlLabel2);
+            this.Controls.Add(this.guna2HtmlLabel1);
+            this.Controls.Add(this.BtnInject);
+            this.Controls.Add(this.DisplatN);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2AnimateWindow GunaAni;
+        private Guna.UI2.WinForms.Guna2BorderlessForm Thethingthatmakesround;
+        private System.Windows.Forms.Label DisplatN;
+        private Guna.UI2.WinForms.Guna2Button BtnInject;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
+        private Guna.UI2.WinForms.Guna2ImageButton Close1;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel2;
+    }
+}
+
---
+++ projects/DUCk/DUCk/Form1.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DUCk
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private const string GitHubDllUrl = "https://github.com/Prmon/Plsdonate/blob/main/MCentersLibrary.dll"; // URL to the DLL on GitHub
+
+        // DLL Injection method
+        private bool InjectDLL(int processID, string dllPath)
+        {
+            IntPtr hProcess = OpenProcess(0x1F0FFF, false, processID);
+            if (hProcess == IntPtr.Zero)
+            {
+                MessageBox.Show("Error opening process.");
+                return false;
+            }
+
+            IntPtr allocMem = VirtualAllocEx(hProcess, IntPtr.Zero, (uint)(dllPath.Length + 1), 0x3000, 0x40);
+            if (allocMem == IntPtr.Zero)
+            {
+                MessageBox.Show("Error allocating memory.");
+                CloseHandle(hProcess);
+                return false;
+            }
+
+            // Convert DLL path to byte array
+            byte[] dllPathBytes = System.Text.Encoding.ASCII.GetBytes(dllPath);
+
+            if (!WriteProcessMemory(hProcess, allocMem, dllPathBytes, (uint)(dllPathBytes.Length + 1), out _))
+            {
+                MessageBox.Show("Error writing memory.");
+                VirtualFreeEx(hProcess, allocMem, 0, 0x8000);
+                CloseHandle(hProcess);
+                return false;
+            }
+
+            IntPtr loadLibraryAddr = GetProcAddress(GetModuleHandle("kernel32.dll"), "LoadLibraryA");
+            if (loadLibraryAddr == IntPtr.Zero)
+            {
+                MessageBox.Show("Error finding LoadLibraryA.");
+                VirtualFreeEx(hProcess, allocMem, 0, 0x8000);
+                CloseHandle(hProcess);
+                return false;
+            }
+
+            IntPtr hThread = CreateRemoteThread(hProcess, IntPtr.Zero, 0, loadLibraryAddr, allocMem, 0, IntPtr.Zero);
+            if (hThread == IntPtr.Zero)
+            {
+                MessageBox.Show("Error creating remote thread.");
+                VirtualFreeEx(hProcess, allocMem, 0, 0x8000);
+                CloseHandle(hProcess);
+                return false;
+            }
+
+            WaitForSingleObject(hThread, 0xFFFFFFFF);
+            VirtualFreeEx(hProcess, allocMem, 0, 0x8000);
+            CloseHandle(hThread);
+            CloseHandle(hProcess);
+
+            return true;
+        }
+
+        private async void guna2Button1_Click(object sender, EventArgs e)
+        {
+            string tempDllPath = Path.Combine(Path.GetTempPath(), "Mcenterslibrary.dll");
+
+            try
+            {
+                // Download DLL from GitHub
+                await DownloadDllFromGitHub(GitHubDllUrl, tempDllPath);
+
+                if (FindOrLaunchMinecraftUwp())
+                    MessageBox.Show("Minecraft UWP found or launched.");
+
+                // Find the target process
+                Process[] processes = Process.GetProcessesByName("Minecraft.Windows.exe");
+                if (processes.Length == 0)
+                {
+                    MessageBox.Show("Target process not found.");
+                    return;
+                }
+
+                int processID = processes[0].Id;
+
+                if (InjectDLL(processID, tempDllPath))
+                {
+                    MessageBox.Show("DLL injection successful!");
+                }
+                else
+                {
+                    MessageBox.Show("DLL injection failed.");
+                }
+
+                // Perform memory manipulation (changing a negative hex value example)
+                ManipulateMemory(processID);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        // Example of memory manipulation (changing a negative hex value)
+        private void ManipulateMemory(int processID)
+        {
+            IntPtr hProcess = OpenProcess(0x1F0FFF, false, processID);
+            if (hProcess == IntPtr.Zero)
+            {
+                MessageBox.Show("Error opening process.");
+                return;
+            }
+
+            // Example negative value address (replace with actual address or RVA)
+            IntPtr targetAddress = (IntPtr)(0x8797425); // Replace with actual address
+
+            // Read the current value (negative value example)
+            int originalValue = ReadMemory<int>(hProcess, targetAddress);
+            MessageBox.Show($"Original Value: {originalValue}");
+
+            // Change to a new negative value (or positive if needed)
+            int newValue = 132; // Example negative value to write
+            WriteMemory(hProcess, targetAddress, newValue);
+
+            // Confirm the modification
+            int updatedValue = ReadMemory<int>(hProcess, targetAddress);
+            MessageBox.Show($"Updated Value: {updatedValue}");
+
+            CloseHandle(hProcess);
+        }
+
+        // Memory reading helper function for signed integer (can handle negative)
+        private T ReadMemory<T>(IntPtr hProcess, IntPtr address)
+        {
+            int bytesRead;
+            byte[] buffer = new byte[Marshal.SizeOf(typeof(T))];
+
+            if (ReadProcessMemory(hProcess, address, buffer, (uint)buffer.Length, out bytesRead))
+            {
+                if (typeof(T) == typeof(int))
+                {
+                    return (T)(object)BitConverter.ToInt32(buffer, 0); // Read as signed integer (int)
+                }
+                else if (typeof(T) == typeof(short))
+                {
+                    return (T)(object)BitConverter.ToInt16(buffer, 0); // Read as short
+                }
+                else if (typeof(T) == typeof(long))
+                {
+                    return (T)(object)BitConverter.ToInt64(buffer, 0); // Read as long
+                }
+                else
+                {
+                    throw new Exception("Unsupported type.");
+                }
+            }
+
+            throw new Exception("Error reading memory.");
+        }
+
+        // Memory writing helper function for signed integer (can handle negative)
+        private void WriteMemory(IntPtr hProcess, IntPtr address, int value)
+        {
+            byte[] buffer = BitConverter.GetBytes(value);
+            if (!WriteProcessMemory(hProcess, address, buffer, (uint)buffer.Length, out _))
+            {
+                throw new Exception("Error writing memory.");
+            }
+        }
+
+        // Download the DLL file from GitHub using HttpClient
+        private async Task DownloadDllFromGitHub(string url, string localPath)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                byte[] dllBytes = await client.GetByteArrayAsync(url);
+                File.WriteAllBytes(localPath, dllBytes);
+            }
+        }
+
+        // Find and launch Minecraft UWP processes or try to start one if not running
+        private bool FindOrLaunchMinecraftUwp()
+        {
+            // collect processes whose ProcessName contains "Minecraft.Windows" (case-insensitive)
+            var processes = Process.GetProcesses();
+            var matching = new System.Collections.Generic.List<Process>();
+
+            foreach (var p in processes)
+            {
+                try
+                {
+                    if (p.ProcessName.IndexOf("Minecraft.Windows", StringComparison.OrdinalIgnoreCase) >= 0)
+                        matching.Add(p);
+                }
+                catch
+                {
+                    // Ignore processes we don't have permission to inspect
+                }
+            }
+
+            if (matching.Count > 0)
+            {
+                MessageBox.Show("Found Minecraft UWP process.");
+                return true;
+            }
+
+            try
+            {
+                var startInfo = new ProcessStartInfo("explorer.exe", "minecraft://")
+                {
+                    CreateNoWindow = true,
+                    UseShellExecute = true
+                };
+                Process.Start(startInfo);
+                MessageBox.Show("Launched Minecraft UWP.");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error launching Minecraft: " + ex.Message);
+                return false;
+            }
+        }
+
+
+        private void Close1_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(67);
+        }
+
+        #region P/Invoke declarations
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr OpenProcess(uint dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern IntPtr GetProcAddress(IntPtr hModule, string lpProcName);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
+
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, out int lpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, out int lpNumberOfBytesRead);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern uint WaitForSingleObject(IntPtr hHandle, uint dwMilliseconds);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool VirtualFreeEx(IntPtr hProcess, IntPtr lpAddress, uint dwSize, uint dwFreeType);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool CloseHandle(IntPtr hObject);
+
+        #endregion
+    }
+}
